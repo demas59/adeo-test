b53ef257acc7d53a22ecc44a98494f183af6ac4f
demas59 <demasay@gmail.com>
2021-11-29 13:15:35 +0100

    Ajout tentative de test + filter on event v1

--- tests-technique/src/main/java/adeo/leroymerlin/cdp/EventService.java
+++ tests-technique/src/main/java/adeo/leroymerlin/cdp/EventService.java
@@ -3,7 +3,10 @@ package adeo.leroymerlin.cdp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class EventService {
@@ -27,9 +30,31 @@ public class EventService {
 
     public List<Event> getFilteredEvents(String query) {
         List<Event> events = eventRepository.findAllBy();
-        // Filter the events list in pure JAVA here
+        // Create value that will store the result of the filtering
+        List<Event> results = new ArrayList<Event>();
+        for(Event event: events){
+            Set<Band> bands = new HashSet<Band>();
+            for(Band band : event.getBands()){
+                Set<Member> members = new HashSet<Member>();
+                for(Member member: band.getMembers()){
+                    if(member.getName().contains(query)){
+                        members.add(member);
+                    }
+                }
+                if(members.size() > 0){
+                    band.setName(band.getName()+" ["+members.size()+"]");
+                    band.setMembers(members);
+                    bands.add(band);
+                }
+            }
+            if(bands.size() > 0){
+                event.setTitle(event.getTitle()+" ["+bands.size()+"]" );
+                event.setBands(bands);
+                results.add(event);
+            }
+        }
 
-        return events;
+        return results;
     }
 
     public Event updateEvent(Event event){
---
+++ tests-technique/src/test/java/adeo/leroymerlin/cdp/EventControllerTest.java
@@ -0,0 +1,5 @@
+package adeo.leroymerlin.cdp;
+
+public class EventControllerTest {
+
+}
